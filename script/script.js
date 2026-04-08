4ee3f2f6e552627f994bbb826b46c77323283d61
KonstantinNikiforov <www.kosta.kon@gmail.com>
2026-04-08 15:50:49 +0300

    add basic requeired validation for the form

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@ const options = menuButton.querySelector(".menu-button__options");
 const applicationSubmitButton = document.getElementById("application-submit-button");
 const dialogContainer = document.getElementById("dialog-container");
 const sendApplicationDialog = document.getElementById("send-application-success-dialog");
+const form = document.querySelector(".send-application__form");
+const appFormItems = form.getElementsByClassName("send-application__form-item");
 
 var isMenuOptionsShown = false;
 
@@ -17,11 +19,10 @@ menuButton.addEventListener("click", () => {
 });
 
 applicationSubmitButton.addEventListener("click", () => {
-    dialogContainer.classList.add("dialog-container--shown");
-    sendApplicationDialog.classList.add("dialog--open");
-});
+    if (!validteForm()) {
+        return;
+    }
 
-applicationSubmitButton.addEventListener("click", () => {
     dialogContainer.classList.add("dialog-container--shown");
     sendApplicationDialog.classList.add("dialog--open");
     const {width, height} = sendApplicationDialog.getBoundingClientRect();
@@ -36,3 +37,41 @@ applicationSubmitButton.addEventListener("click", () => {
         sendApplicationDialog.classList.remove("dialog--open");
     }, { once: true });
 });
+
+for (let item of appFormItems) {
+    const input = item.querySelector(".input");
+
+    input.addEventListener("input", (event) => {
+        validateInput(item.querySelector(".input"), item);
+    });
+}
+
+function validteForm() {
+    var isValidResult = true;
+
+    for (let item of appFormItems) {
+        const isValid = validateInput(item.querySelector(".input"), item);
+
+        if (!isValid) {
+            isValidResult = false;
+        }
+    }
+
+    return isValidResult;
+}
+
+function validateInput(input, item) {
+    if (isValidValue(input.value)) {
+        item.classList.remove("send-application__form-item--invalid");
+
+        return true;
+    } else {
+        item.classList.add("send-application__form-item--invalid");
+
+        return false;
+    }
+}
+
+function isValidValue(value) {
+    return !!value?.length;
+}
