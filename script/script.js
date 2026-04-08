a1b32cc044613e0e892929a4f901bfd5b98ba54c
KonstantinNikiforov <www.kosta.kon@gmail.com>
2026-04-08 15:27:19 +0300

    add dialog; fix navigation;

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const menuButton = document.getElementById("menu-button");
-
 const options = menuButton.querySelector(".menu-button__options");
+const applicationSubmitButton = document.getElementById("application-submit-button");
+const dialogContainer = document.getElementById("dialog-container");
+const sendApplicationDialog = document.getElementById("send-application-success-dialog");
 
 var isMenuOptionsShown = false;
 
@@ -12,4 +14,25 @@ menuButton.addEventListener("click", () => {
     } else {
         options.style.display = "none";
     }
+});
+
+applicationSubmitButton.addEventListener("click", () => {
+    dialogContainer.classList.add("dialog-container--shown");
+    sendApplicationDialog.classList.add("dialog--open");
+});
+
+applicationSubmitButton.addEventListener("click", () => {
+    dialogContainer.classList.add("dialog-container--shown");
+    sendApplicationDialog.classList.add("dialog--open");
+    const {width, height} = sendApplicationDialog.getBoundingClientRect();
+
+    sendApplicationDialog.style.left = `calc(50% - ${width / 2}px)`
+    sendApplicationDialog.style.top = `calc(50% - ${height / 2}px)`
+
+    const primarryButton = sendApplicationDialog.querySelector(".dialog__primarry-button");
+
+    primarryButton.addEventListener("click", () => {
+        dialogContainer.classList.remove("dialog-container--shown");
+        sendApplicationDialog.classList.remove("dialog--open");
+    }, { once: true });
 });
